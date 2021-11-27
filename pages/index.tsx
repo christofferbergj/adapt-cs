bcbe2d0856ba0f6a39a507d8ab144ce51600a7a0
Christoffer Berg <christofferbergj@gmail.com>
2021-11-27 14:32:29 +0100

    Transpiled mainframe.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h2>hello from Home</h2>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/elements/Button.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+  return (
+    <div>
+      <h2>Hello from Button</h2>
+    </div>
+  )
+}
