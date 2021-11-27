218d44cbd73c94486cfad4cf943349265612ee6f
Christoffer Berg <christofferbergj@gmail.com>
2021-11-27 14:53:01 +0100

    hey, what's that over there?!

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,7 +1,9 @@
-export const Button = () => {
-  return (
-    <div>
-      <h2>Hello from Button</h2>
-    </div>
-  )
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+
+export const Button = ({ children }: Props) => {
+  return <button>{children}</button>
 }
