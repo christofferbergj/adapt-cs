bf5865331589be5256c56eb6d419e0c97736ca02
Christoffer Berg <christofferbergj@gmail.com>
2021-11-29 11:22:37 +0100

    Give me a break, it's 2am.  But it works now.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { NextApiHandler } from 'next'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
+import invariant from 'tiny-invariant'
 import NextAuth from 'next-auth'
 import type { NextAuthOptions } from 'next-auth'
 
@@ -15,6 +16,24 @@ const options: NextAuthOptions = {
     }),
   ],
   secret: process.env.SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      return !!user.email?.endsWith('adaptagency.com')
+    },
+    async session({ session, user }) {
+      const prismaUser = await prisma.user.findUnique({
+        where: {
+          id: user.id,
+        },
+      })
+
+      invariant(prismaUser, 'User does not exist')
+
+      session.user = prismaUser
+
+      return session
+    },
+  },
 }
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
