e3d57cd68ee6c75ed8ec23c74b0d28797337a617
Christoffer Berg <christofferbergj@gmail.com>
2021-12-04 16:43:48 +0100

    Love coding? here's the secret reason why

--- src/application/ports.ts
+++ src/application/ports.ts
@@ -1,7 +1,7 @@
 import type { Fine } from '@entities/fine/types'
 
 export interface FineService {
-  createFine(fine: Omit<Fine, 'id'>): Promise<Fine>
+  createFine(fine: Omit<Fine, 'id'>): Promise<Fine | null>
   getFines(): Promise<Fine[]>
   // deleteFine(id: Fine['id']): Promise<Fine>
   // getFine(id: Fine['id']): Promise<Fine>
