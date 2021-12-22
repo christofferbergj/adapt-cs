4074200b4e54434935818fd1ed3271475298fba8
Christoffer Berg <christofferbergj@gmail.com>
2021-12-23 00:23:45 +0100

    this should fix it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,8 +37,8 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_API_URL
+      ? process.env.NEXT_PUBLIC_API_URL
       : 'http://localhost:3000/api/trpc'
 
     return {
